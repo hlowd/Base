2d86ccb722ee2ce5f6b754061e1001dcd58a9319
zf <tosee@yeah.com>
2022-12-09 14:04:03 +0800

    add some err return

--- src/Error.php
+++ src/Error.php
@@ -7,6 +7,7 @@ class Error{
     const FAIL = 1;     //通用失败
     const NO_LOGIN = 2; //没有登录
     const NO_PRIV = 3;  //没有操作权限
+    const NO_ORDER = 4;  //没有课程权限
 
     public static function succ($data=[],$msg="成功"){
         return ['Code'=>self::SUCCESS,'Data'=>$data,'Msg'=>$msg];
@@ -20,6 +21,16 @@ class Error{
         return ['Code'=>self::FAIL,'Msg'=>$msg];
     }
 
+    public static function failNoLogin(){
+        return ['Code'=>self::NO_LOGIN,'Msg'=>'您还没有登录!'];
+    }
 
+    public static function failNoPriv(){
+        return ['Code'=>self::NO_PRIV,'Msg'=>'您没有相关操作权限！'];
+    }
+
+    public static function failNoOrder(){
+        return ['Code'=>self::NO_ORDER,'Msg'=>'您没有相关学习权限，请购买!'];
+    }
 
 }
