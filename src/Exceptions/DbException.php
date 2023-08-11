01f187f57252fc6b5a541ea150f1ccfbd802428a
zz <zf@1.com>
2023-08-11 17:15:53 +0800

    增加几个异常

---
+++ src/Exceptions/DbException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Hlowd\Base\Exceptions;
+use Exception;
+
+class DbException extends Exception
+{
+
+}
---
+++ src/Exceptions/RedisException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Hlowd\Base\Exceptions;
+
+use Exception;
+
+class RedisException extends Exception
+{
+
+}
