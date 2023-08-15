42a80f099a12049b59b18aed0533191aff4699fa
zz <zf@1.com>
2023-08-15 14:44:27 +0800

    111

--- src/Facade/Validator.php
+++ src/Facade/Validator.php
@@ -11,6 +11,12 @@ use Hlowd\Base\Service\ValidatorClass;
  * @method static bool isEmail(string $str)
  * @method static bool isSms(string $str)
  * @method static bool isIdNum(string $str)
+ *
+ * @method static string filterMobile(string $str)
+ * @method static string filterPassword(string $str)
+ * @method static string filterEmail(string $str)
+ * @method static string filterSms(string $str)
+ * @method static string filterIdNum(string $str)
  */
 
 
--- src/Service/ValidatorClass.php
+++ src/Service/ValidatorClass.php
@@ -11,30 +11,62 @@ class ValidatorClass implements IValidator
         return $this->regCheck($str,'/^1[3456789]\d{9}$/');
     }
 
+    public function filterMobile(string $str=''):bool{
+        return $this->regFilter($str,'/^1[3456789]\d{9}$/');
+    }
+
     public function isPassword(string $str=''):bool{
         return $this->regCheck($str,'/^(?=.*\d)(?=.*[a-zA-Z])(?=.*[^\da-zA-Z\s]).{8,20}$/');
+    }
+
+    public function filterPassword(string $str=''):bool{
+        return $this->regFilter($str,'/^(?=.*\d)(?=.*[a-zA-Z])(?=.*[^\da-zA-Z\s]).{8,20}$/');
+    }
+
+    public function filterNormalPassword(string $str=''):bool{
+        return $this->regFilter($str,'/\w.{8,20}$/');
     }
 
     public function isIdNum(string $str=''):bool{
         return $this->regCheck($str,'/(^\d{15}$)|(^\d{18}$)|(^\d{17}(\d|X|x)$)/');
     }
 
+    public function filterIdNum(string $str=''):bool{
+        return $this->regFilter($str,'/(^\d{15}$)|(^\d{18}$)|(^\d{17}(\d|X|x)$)/');
+    }
+
+
     public function isSms(string $str=''):bool{
         return $this->regCheck($str,'/^\d{4}$/');
     }
-    /**
-     * @param string $str
-     * @return bool
-     */
+
+    public function filterSms(string $str=''):bool{
+        return $this->regFilter($str,'/^\d{4}$/');
+    }
+
+
     public function isEmail(string $str=''):bool{
         return $this->regCheck($str,'/^\w+[-|\w._]+@(\w+(-\w+)?\\.)+[a-z]{2,}$/');
     }
+
+    public function filterEmail(string $str=''):bool{
+        return $this->regFilter($str,'/^\w+[-|\w._]+@(\w+(-\w+)?\\.)+[a-z]{2,}$/');
+    }
+
 
     private function regCheck(string $str,string $reg):bool{
         if (preg_match($reg, $str, $matches)) {
             return boolval($matches[0]);
         }else{
             return false;
+        }
+    }
+
+    private function regFilter(string $str,string $reg):string{
+        if (preg_match($reg, $str, $matches)) {
+            return $matches[0];
+        }else{
+            return '';
         }
     }
 
